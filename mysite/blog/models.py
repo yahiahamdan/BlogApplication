4e94fac761bfd6932bf4ea5df328033ec6de85f1
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-07 02:48:41 +0300

    creating model manager for published post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,10 @@ from  django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import User
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF','Draft'
@@ -20,6 +24,8 @@ class Post(models.Model):
     status=models.CharField(max_length=2,
     
                         choices=Status.choices,default=Status.DRAFT)
+    objects=models.Manager()
+    PublishedM=PublishedManager()
     class Meta:
         ordering=['-publish']
         indexes=[
