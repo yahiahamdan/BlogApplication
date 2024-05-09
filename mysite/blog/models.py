2b5f1a2f02085113a6ecf82e275a62c4d553febc
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-09 10:05:39 +0300

    updating the model by adding addtional property to the slug

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from  django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.urls import reverse 
+from django.urls import reverse
+from django.http import HttpResponseRedirect 
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -16,7 +17,8 @@ class Post(models.Model):
     title=models.CharField(max_length=250)
     publish=models.DateTimeField(default=timezone.now)
 
-    slug=models.SlugField(max_length=250,unique_for_date=publish)
+    slug=models.SlugField(max_length=250,unique_for_date='publish')
+
     author=models.ForeignKey(User,on_delete=models.CASCADE,
                              related_name='blog_posts'   
                              )
@@ -36,5 +38,10 @@ class Post(models.Model):
     def __str__(self) :
         return self.title
     def get_absolute_url(self):
-        return reverse(" blog:post_detail",args=[self.id])
-    
+        return  reverse("blog:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+              ])
+        
