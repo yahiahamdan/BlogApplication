0f3c6b9c087bf7490bdc9dcbf55af5177db91b88
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-09 19:31:41 +0300

    adding model form for comments to make comments

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from .models import Comments
 class EmailPostForm(forms.Form):
     name=forms.CharField(max_length=25)
     email=forms.EmailField()
     to=forms.EmailField()
     comments=forms.CharField(required=False,widget=forms.Textarea)
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comments
+        fields=['name','email','body']
+        
