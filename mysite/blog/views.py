ef98229ae589b55b29cec9d26729518ea26fb9fd
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-09 11:58:48 +0300

    adding pagiation logic in the view and handling different erros as (pagenotinteger,emptypage)

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .models import Post
 # Create your views here.
 
 
 def post_list(request):
-    post=Post.PublishedM.all()
+    post_list=Post.PublishedM.all()
+    paginator=Paginator(post_list,2)
+    page_number=request.GET.get('page',1)
+    try:
+     posts=paginator.page(page_number)
+    except PageNotAnInteger:
+     posts=paginator.page(1)
+    except EmptyPage:
+     posts=paginator.page(paginator.num_pages)
+       
     return render(request,'blog/post/list.html',
-                  {'posts':post}   
+                  {'posts':posts}   
                   )
 
 def post_detail(request,year,month,day,post):
