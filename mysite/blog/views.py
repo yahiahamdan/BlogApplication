20e73a1f0610dc269ae4ede6f27fb27982877e3d
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-09 19:33:22 +0300

    by adding comment class fixing the comment form with the detail.html and  updating the url

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import Http404,HttpRequest
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from .models import Post
+from .models import Post,Comments
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 class PostListView(ListView):
@@ -16,6 +17,21 @@ class PostListView(ListView):
   context_object_name='posts'
   paginate_by=3
   template_name='blog/post/list.html'
+@require_POST
+def post_comment(request,post_id):
+   post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+   comment = None
+   form=CommentForm(data=request.POST)
+   if(form.is_valid):
+     comment=form.save(commit=False)
+     comment.post=post
+     comment.save()
+   return render(request,'blog/post/comment.html',
+                  {'post':post,
+                   'form':form,
+                   'comment':comment
+                   })
+
 
 def post_share(request,post_id):
   post=get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
@@ -67,4 +83,6 @@ def post_detail(request,year,month,day,post):
                           publish__month=month,
                           publish__day=day
                            )
-    return render(request,'blog/post/detail.html',{'post':post})
+    comments=post.comments.filter(active=True)
+    form=CommentForm()
+    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'form':form})
