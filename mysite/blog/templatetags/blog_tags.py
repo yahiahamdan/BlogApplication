6b205bf291e73a3df3b08741a19705a5189d67ad
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-11 12:09:26 +0300

    doing custom tags folder called templatetags and simple_tag to get all the count of published posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -5,7 +5,7 @@ register=template.Library()
 @register.simple_tag
 def total_post():
     return Post.PublishedM.count()
-@register.inclusion_tag('blog/post/lastest_posts.html')
-def show_lastest_posts(count=5):
-    lastest_posts=Post.PublishedM.order_by('-publish')[:count]
-    return {'lastest_Posts':lastest_posts}
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts=Post.PublishedM.order_by('-publish')[:count]
+    return {'last_post':latest_posts}
