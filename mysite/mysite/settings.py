1904790df611995c62308800db84f1a0ea6b0ded
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-11 18:19:43 +0300

    adding postqresql instead of sqlite to provide full search engine and providing mysite_data.json to shift the data to postgresql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
      'taggit',
      'django.contrib.sites',
      'django.contrib.sitemaps',
+     'django.contrib.postgres',
 ]
 #email server configuration 
 EMAIL_HOST='smtp.gmail.com'
@@ -86,8 +87,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER':'postgres',
+        'PASSWORD':'yahia'
     }
 }
 
