4482ad11755acda345899b314d447969ac11322d
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-11 14:43:22 +0300

    adding sitempas class and update url to see the sitemap xml

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+sitemaps = {
+ 'posts': PostSitemap,
+}
 app_name='blog'
 urlpatterns = [
     path('',views.post_list,name='post_list'),
@@ -8,6 +13,9 @@ urlpatterns = [
    
     path('<int:year>/<int:month>/<int:day>/<slug:post>',views.post_detail,name='post_detail'), 
     path('<int:post_id>/share',views.post_share,name='post_share'),
-    path('<int:post_id>/comment/',views.post_comment,name="post_comment")
+    path('<int:post_id>/comment/',views.post_comment,name="post_comment"),
+     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+ name='django.contrib.sitemaps.views.sitemap')
 ]
+
 
