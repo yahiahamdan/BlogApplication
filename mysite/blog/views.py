c11c49558ae4d40770ddf0338b1de8ebd6413bcf
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-11 11:02:53 +0300

    implementing recomendation based on tags to get similar posts order by tags , -publish

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,6 +8,7 @@ from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 
 class PostListView(ListView):
@@ -81,11 +82,6 @@ def post_list(request,tag_slug=None):
                   )
 
 def post_detail(request,year,month,day,post):
-    # try:
-    #     post=Post.PublishedM.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("no post found with this id")
-    # return render(request,'blog/post/detail.html',{'post':post})
     post=get_object_or_404(Post,
                           status= Post.Status.PUBLISHED,
                           slug=post,
@@ -95,4 +91,11 @@ def post_detail(request,year,month,day,post):
                            )
     comments=post.comments.filter(active=True)
     form=CommentForm()
-    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'form':form})
+   #list of similar posts 
+    post_tags_ids=post.tags.values_list('id',flat=True)
+    similar_posts=Post.PublishedM.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts=similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'form':form
+                                                  , 'similar_posts':similar_posts
+                                          })
